530f5e38abbbe0da35ff1f46545c6c68ec4796cb
Brown <BrownS7@mskcc.org>
2021-06-14 12:25:32 -0400

    revise toy analysis

--- toy_data_os_dx.R
+++ toy_data_os_dx.R
@@ -1,6 +1,6 @@
 #############################################################################################
 ######### Evaluating Delayed Study Entry & the Effect of Left Truncation Adjustment ######### 
-######### in Overall Survival from Diagnosis from a Hypotheical Data Set ####################
+############# in Overall Survival from Diagnosis from a Toy Data Set ########################
 #############################################################################################
 
 ## Load libraries
@@ -9,7 +9,7 @@ library(survival)
 library(survminer)
 
 ## Load data
-hyp_data = read.csv(here::here("hypothetical_data.csv"))
+toy_data = read.csv(here::here("toy_data.csv"))
 
 ## Variables of interest
 ## os_status_dx = patient's overall survival status (1 = dead; 0 = censored)
@@ -18,15 +18,15 @@ hyp_data = read.csv(here::here("hypothetical_data.csv"))
 ## tt_cpt_report_mos = time (months) from date of cancer diagnosis to date of genomic sequencing
 
 ## Calculate median overall survival from diagnosis for total population
-survfit(Surv(tt_os_dx_mos, os_status_dx) ~ 1, data = hyp_data)
+survfit(Surv(tt_os_dx_mos, os_status_dx) ~ 1, data = toy_data)
 
 ## Subset data on those who underwent genomic sequencing
-hyp_data_subset = hyp_data %>% 
+toy_data_subset = toy_data %>% 
   filter(cpt_status == 1)
 
 ## Calculate unadjusted median overall survival from diagnosis for subsetted population
-survfit(Surv(tt_os_dx_mos, os_status_dx) ~ 1, data = hyp_data)
+survfit(Surv(tt_os_dx_mos, os_status_dx) ~ 1, data = toy_data_subset)
 
 ## Calculate median overall survival from diagnosis for subsetted population after left truncation adjustment
-survfit(Surv(time = tt_cpt_report_mos, time2 = tt_os_dx_mos, os_status_dx) ~ 1, data = hyp_data)
+survfit(Surv(time = tt_cpt_report_mos, time2 = tt_os_dx_mos, os_status_dx) ~ 1, data = toy_data_subset)
 
